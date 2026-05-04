bbed9248fea390dcb289fe99fc941bcd2cb4d57b
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-05-04 12:56:40 +0000

    querySelector

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+// Boolean variable to track mode
+let darkMode = false;
+
+/* NEW FUNCTION: document.querySelector
+   lets you select any HTML element
+   using its tag, class, or id
+*/
+
+// const (instead of let) because we're storing a REFERENCE to part of the HTML page
+const body = document.querySelector("body");
+const heading = document.querySelector("#main-heading");
+const description = document.querySelector("#description");
+const toggleBtn = document.querySelector("#toggle");
+
+// Now we can change styles & attributes for those elements
+heading.style.color = "purple"; // JS wrote CSS code...
+heading.textContent = "Light Mode & Dark Mode Website" ; 
