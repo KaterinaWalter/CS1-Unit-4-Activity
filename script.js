42bb85196082f597643051c1da4cbf392de5ca97
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-05-07 15:55:17 +0000

    toggleMode function

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ let darkMode = false;
 
 /* NEW JS FUNCTION: document.querySelector()
    lets us SELECT an HTML ELEMENT and store
-   a reference to it in a VARIABLE
+   a reference (pointer) to it in a VARIABLE
 */
-
-// select by tag type
 const body = document.querySelector("body");
-// heading is a VARIABLE that points to the h1 ELEMENT with an id="main-heading" -> just like CSS selection!
+
+// heading is a VARIABLE that points to the h1 ELEMENT with id="main-heading" -> just like CSS selection!
 const heading = document.querySelector("#main-heading");
 const description = document.querySelector("#description");
 const toggleBtn = document.querySelector("#toggle");
@@ -17,4 +16,26 @@ const toggleBtn = document.querySelector("#toggle");
 heading.textContent = "Light & Dark Mode Website"; 
 heading.style.color = "purple"; 
 
+// Define what happens when button is clicked
+function toggleMode() {
+   console.log("triggered function");
+   // 1. Flip the boolean to reflect the mode
+   darkMode = !darkMode; 
+   console.log(darkMode);
+   // 2. Conditionally apply styles based on mode
+   if (darkMode == true) {
+      console.log("apply dark styles");
+      body.style.background = "rgb(0,0,0)";
+      heading.style.color = "lightgrey";
+      description.style.color = "white";
+      description.textContent = "Welcome to the dark side 🌚";
+      toggleBtn.textContent = "Switch to Light";
+   }
+   else {
+      console.log("apply light styles");
+      body.style.background = "white";
+   }
 
+}
+// Attach function to the actual button
+toggleBtn.addEventListener("click", toggleMode);
